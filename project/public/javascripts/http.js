db415c5ff816d19a66c4b2ead364d97e8af7934d
Neriya Malachi <neriamalachi@gmail.com>
2024-02-04 14:50:25 +0200

    .

--- project/public/javascripts/http.js
+++ project/public/javascripts/http.js
@@ -12,7 +12,7 @@ const server = http.createServer((req,res) => {
         return  res.end();
     
     }    
-    console.log(data);
+    // console.log(data);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data.toString());
     res.end();
